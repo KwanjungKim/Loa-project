0078d81fa1197e5a89ed79cd83867a0c45982e0e
Wille Lee <1992season@gmail.com>
2023-11-02 15:13:37 +0900

    [ADD] added packages and set router-dom, recoil test page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import TestRoute from "./routes/Test";
+import HomeRoute from "./routes/HomeRoute";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomeRoute />,
+      },
+      {
+        path: "/test",
+        element: <TestRoute />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
