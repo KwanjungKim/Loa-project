20701c34412b630e4a06a2d80919d7607ca09bfb
Wille Lee <1992season@gmail.com>
2023-11-07 14:39:00 +0900

    [ADD] added material ui

---
+++ client/src/routes/Test/Components.tsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+const TestComponents = () => {
+  return (
+    <div>
+      <Button variant="contained">hello test</Button>
+    </div>
+  );
+};
+
+export default TestComponents;
