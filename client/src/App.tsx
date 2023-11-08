4b15f5fe034b32ab1ddba56075595ff291539cc8
Wille Lee <1992season@gmail.com>
2023-11-08 18:32:57 +0900

    [ADD] init modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { RouterProvider } from "react-router-dom";
-
-// recoil
-
-// router
-import router from "./router";
 import useTheme from "./hooks/useTheme";
+import router from "./router";
 
 const App = () => {
   const theme = useTheme();
