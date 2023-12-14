4609fcf49deb782350c26576486dbce556525884
Wille Lee <1992season@gmail.com>
2023-12-14 19:03:18 +0900

    [ADD] added swr case to pingpong

--- frontend/src/routes/Test/PingPongRoute.tsx
+++ frontend/src/routes/Test/PingPongRoute.tsx
@@ -1,7 +1,15 @@
+import useSWR from "swr";
 import { SmallButton } from "../../components/common/Button";
-import fetchUtils from "../../utils/fetchUtils";
+import fetchUtils, { IResponseData } from "../../utils/fetchUtils";
 
 const PingPongRoute = () => {
+  const { data, isLoading } = useSWR<IResponseData>(
+    "/api/ping",
+    (url: string) => {
+      return fetchUtils.post(url, { id: "ping" });
+    },
+  );
+
   const testPingPong1 = async () => {
     const response = await fetch("http://localhost:8080/api/ping", {
       method: "POST",
@@ -13,20 +21,29 @@ const PingPongRoute = () => {
     const data = await response.json();
     console.log(data);
   };
+
   const testPingPong2 = async () => {
     const response = await fetchUtils.post("/api/ping", {
       id: "ping",
     });
     console.log(response);
   };
+
   return (
     <div>
-      <SmallButton variant="contained" onClick={testPingPong1}>
-        Ping 1
-      </SmallButton>
-      <SmallButton variant="contained" onClick={testPingPong2}>
-        Ping 2
-      </SmallButton>
+      <div>
+        <SmallButton variant="contained" onClick={testPingPong1}>
+          Ping 1
+        </SmallButton>
+        <SmallButton variant="contained" onClick={testPingPong2}>
+          Ping 2
+        </SmallButton>
+      </div>
+      <br />
+      <div>
+        <h3>ping response by SWR &darr;</h3>
+        <p>{isLoading ? "loading swr..." : JSON.stringify(data)}</p>
+      </div>
     </div>
   );
 };
