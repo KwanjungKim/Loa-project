d5ced03b0d152392920ea5eabc2a8cda97ee3b6d
hongsi <krs1994@naver.com>
2024-02-21 22:14:09 +0900

    add useform

--- frontend/src/components/kakaoLogin/testsaramin.tsx
+++ frontend/src/components/kakaoLogin/testsaramin.tsx
@@ -8,7 +8,15 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
 import { selectList, selectRaidType } from "./saraminData";
 import fetchUtils from "../../utils/fetchUtils";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
+interface FormValue {
+  title: string;
+  raid_difficulty: string;
+  gate?: number;
+}
 type IparamMap = {
   title: string; //  글제목
   content: string; // 글 내용
@@ -18,8 +26,8 @@ type IparamMap = {
   raid_difficulty: string; // 레이드 난이도
   raid_type: string; // 레이드 타입
   proficiency: string; // 숙련도
-  minGate: string; // 최소 관문
-  maxGate: string; // 최대 관문
+  minGate: number; // 최소 관문
+  maxGate: number; // 최대 관문
   card_level: string; // 카드 레벨
   startDate: string; // 출발 시간
   member: string[];
@@ -68,10 +76,10 @@ const TestSaramIn = () => {
     character_name: mainCharacter.character_name,
     raid_leader: mainCharacter.character_name,
     raid_difficulty: "",
-    raid_type: "",
-    proficiency: "",
-    minGate: "",
-    maxGate: "",
+    raid_type: "발탄",
+    proficiency: "트라이",
+    minGate: 1,
+    maxGate: 1,
     card_level: "조건 없음",
     startDate: "",
     member: [mainCharacter.character_name],
@@ -80,7 +88,7 @@ const TestSaramIn = () => {
 
   // title, diff, raid type, gate, content
   // input data => paramMap
-  const handleData = (data: string, e: string) => {
+  const handleData = (data: string, e: any) => {
     setParamMap((prev) => {
       return {
         ...prev,
@@ -163,112 +171,143 @@ const TestSaramIn = () => {
     });
   };
 
+  const schema = yup.object().shape({
+    title: yup.string().required("제목을 입력해주세요,"),
+    raid_difficulty: yup.string().required("레이드 난이도를 선택해주세요."),
+    gate: yup
+      .number()
+      .min(paramMap.minGate, "최대관문이 최소관문보다 낮습니다."),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValue>({
+    mode: "onChange",
+    reValidateMode: "onChange",
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmitHandler: SubmitHandler<FormValue> = () => {
+    addArticle();
+  };
   return (
-    <div>
+    <>
       <button onClick={() => console.log(paramMap)}>123</button>
-      <input
-        placeholder="제목을 입력해주세요."
-        type="text"
-        maxLength={50}
-        onChange={(e) => handleData("title", e.target.value)}
-      />{" "}
-      <br />
-      <LocalizationProvider adapterLocale="en" dateAdapter={AdapterDayjs}>
-        <DateTimePicker
-          label="출발 시간을 선택해주세요."
-          format="YYYY년 MM월 DD일 A HH시 mm분"
-          defaultValue={null}
-          slotProps={{
-            textField: {
-              size: "small",
-            },
-          }}
-          onChange={(newValue) => {
-            handleDateChange("startDate", newValue);
-          }}
-          sx={{
-            width: "305px",
-            margin: "10px",
-          }}
+      <form onSubmit={handleSubmit(onSubmitHandler)}>
+        <input
+          placeholder="제목을 입력해주세요."
+          type="text"
+          maxLength={50}
+          {...register("title")}
+          onChange={(e) => handleData("title", e.target.value)}
         />
-      </LocalizationProvider>
-      <br />
-      <input
-        type="radio"
-        name="RaidDiff"
-        value="normal"
-        onChange={(e) => handleData("raid_difficulty", e.target.value)}
-      />{" "}
-      Normal
-      <input
-        type="radio"
-        name="RaidDiff"
-        value="hard"
-        onChange={(e) => handleData("raid_difficulty", e.target.value)}
-      />{" "}
-      Hard
-      <input
-        type="radio"
-        name="RaidDiff"
-        value="extream"
-        onChange={(e) => handleData("raid_difficulty", e.target.value)}
-      />{" "}
-      Extream
-      <br />
-      레이드 종류
-      <select
-        onChange={(e) => {
-          handleData("raid_type", e.target.value);
-          setGate(e.target.selectedIndex);
-        }}
-      >
-        {selectRaidType.raidtype.map((value) => (
-          <>
-            <option key={value.id}>{value.name}</option>
-          </>
-        ))}
-      </select>{" "}
-      <br />
-      최소
-      <select onChange={(e) => handleData("minGate", e.target.value)}>
-        {gateFilter().map((value: number, id: number) => (
-          <option key={id}>{value}관문</option>
-        ))}
-      </select>
-      ~ 최대
-      <select onChange={(e) => handleData("maxGate", e.target.value)}>
-        {gateFilter().map((value: number, id: number) => (
-          <option key={id}>{value}관문</option>
-        ))}
-      </select>
-      <br />
-      숙련도
-      <HandleSelect str={"proficiency"} setParamMap={setParamMap} />
-      카드
-      <HandleSelect str={"card_level"} setParamMap={setParamMap} />
-      <br />
-      세부사항 <br />
-      <textarea
-        aria-label="dkd"
-        onChange={(e) => handleData("content", e.target.value)}
-        placeholder="세부사항을 적어주세요."
-        style={{ width: "400px", height: "300px", resize: "none" }}
-      />
-      <br />
-      {paramMap.member[0]} {"  "}
-      {paramMap.member[1]} {"  "}
-      {paramMap.member[2]} {"  "}
-      {paramMap.member[3]} {"  "}
-      <br />
-      {paramMap.member[4]} {"  "}
-      {paramMap.member[5]} {"  "}
-      {paramMap.member[6]} {"  "}
-      {paramMap.member[7]} {"  "} <br />
-      <input onChange={(e) => setCharacterName(e.target.value)} />
-      <button onClick={() => getCharacter()}>getCharacter</button>
-      <br />
-      <button onClick={() => addArticle()}>글 작성</button>
-    </div>
+        <p>{errors.title?.message}</p> <br />
+        <LocalizationProvider adapterLocale="en" dateAdapter={AdapterDayjs}>
+          <DateTimePicker
+            label="출발 시간을 선택해주세요."
+            format="YYYY년 MM월 DD일 A HH시 mm분"
+            defaultValue={null}
+            slotProps={{
+              textField: {
+                size: "small",
+              },
+            }}
+            onChange={(newValue) => {
+              handleDateChange("startDate", newValue);
+            }}
+            sx={{
+              width: "305px",
+              margin: "10px",
+            }}
+          />
+        </LocalizationProvider>
+        <br />
+        <input
+          type="radio"
+          value="normal"
+          {...register("raid_difficulty")}
+          onChange={(e) => handleData("raid_difficulty", e.target.value)}
+        />{" "}
+        Normal
+        <input
+          type="radio"
+          value="hard"
+          {...register("raid_difficulty")}
+          onChange={(e) => handleData("raid_difficulty", e.target.value)}
+        />{" "}
+        Hard
+        <input
+          type="radio"
+          value="extream"
+          {...register("raid_difficulty")}
+          onChange={(e) => handleData("raid_difficulty", e.target.value)}
+        />{" "}
+        Extream
+        {errors.raid_difficulty?.message}
+        <br />
+        레이드 종류
+        <select
+          onChange={(e) => {
+            handleData("raid_type", e.target.value);
+            setGate(e.target.selectedIndex);
+          }}
+        >
+          {selectRaidType.raidtype.map((value) => (
+            <>
+              <option key={value.id}>{value.name}</option>
+            </>
+          ))}
+        </select>{" "}
+        <br />
+        최소
+        <select onChange={(e) => handleData("minGate", e.target.value)}>
+          {gateFilter().map((value: number, id: number) => (
+            <option key={id}>{value}</option>
+          ))}
+        </select>
+        관문 ~ 최대
+        <select
+          {...register("gate")}
+          onChange={(e) => handleData("maxGate", e.target.value)}
+        >
+          {gateFilter().map((value: number, id: number) => (
+            <option key={id}>{value}</option>
+          ))}
+        </select>
+        관문 <br />
+        {errors.gate?.message}
+        <br />
+        숙련도
+        <HandleSelect str={"proficiency"} setParamMap={setParamMap} />
+        카드
+        <HandleSelect str={"card_level"} setParamMap={setParamMap} />
+        <br />
+        세부사항 <br />
+        <textarea
+          placeholder="세부사항을 적어주세요."
+          style={{ width: "400px", height: "300px", resize: "none" }}
+          onChange={(e) => handleData("content", e.target.value)}
+        />
+        <br />
+        {paramMap.member[0]} {"  "}
+        {paramMap.member[1]} {"  "}
+        {paramMap.member[2]} {"  "}
+        {paramMap.member[3]} {"  "}
+        <br />
+        {paramMap.member[4]} {"  "}
+        {paramMap.member[5]} {"  "}
+        {paramMap.member[6]} {"  "}
+        {paramMap.member[7]} {"  "} <br />
+        <input onChange={(e) => setCharacterName(e.target.value)} />
+        <button onClick={() => getCharacter()}>getCharacter</button>
+        <br />
+        <div>
+          <input type="submit" value={"글작성"} />
+        </div>
+      </form>
+    </>
   );
 };
 
