bd60d4dcc02a9555a005a4bb338f9d086e4d20a8
Wille Lee <1992season@gmail.com>
2024-03-09 23:01:32 +0900

    [ADD] my calendar

--- frontend/src/components/MyCalendar/index.tsx
+++ frontend/src/components/MyCalendar/index.tsx
@@ -1,169 +1,52 @@
-import { useCallback, useEffect, useState } from "react";
-import dayjs from "dayjs";
-import { useRecoilValue } from "recoil";
-import { mainCharState } from "../../atoms/mainCharacter";
-import fetchUtils from "../../utils/fetchUtils";
-
-// import Calendar from "../common/Calendar";
-// import dayjs from "dayjs";
-
-interface IDate {
-  date: string;
-  start: boolean;
-  data: any[];
-}
-
-const dates: IDate[] = [];
-let month: number = 0;
-
-for (let i = 0; i < 30; i++) {
-  const date = dayjs().add(i, "day");
-  const m = date.month();
-  const item = {
-    date: dayjs().add(i, "day").format("YYYY-MM-DD"),
-    start: m !== month,
-    data: [],
-  };
-  dates.push(item);
-  month = m;
-}
-
-// 7 items in one turn
-const lastTurn = Math.floor(dates.length / 7);
-// console.log("lastTurn", lastTurn);
+import { useCallback, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import useMyArticles from "../../hooks/useMyArticles";
+import MyCalendarView, {
+  MyCalendarViewDatesProps,
+  MyCalendarViewListProps,
+} from "./MyCalendarView";
 
 const MyCalendar = () => {
-  /*
-  const highlighted: string[] = [];
-  const [date, setDate] = useState<dayjs.Dayjs | null>(null);
-  */
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [dateList, setDateList] = useState<IDate[]>(dates);
-  console.log("dateList", dateList);
-  const isMainChar = useRecoilValue(mainCharState);
-  const [dateTurn, setDateTurn] = useState(0);
+  const navigate = useNavigate();
 
-  const selectedDates = dateList.slice(dateTurn * 7, dateTurn * 7 + 7);
-  const [selectedDate, setSelectedDate] = useState("");
-  console.log("selectedDate", selectedDate);
+  const {
+    handleNextTurn,
+    handlePrevTurn,
+    selectedDates,
+    listBySelectedDate,
+    handleSelectDate,
+  } = useMyArticles();
 
-  const getData = useCallback(async () => {
-    if (!isMainChar.user_number) return;
-    const { data, success } = await fetchUtils.post("/board/getRaid", {
-      user_number: isMainChar.user_number,
-    });
-    if (!success) return;
-    if (!dateList) return;
-    console.log("data", data);
-    const tempList: { date: string; list: any[] }[] = [];
-    console.log(dateList);
-    data.boardModel.board_list.map((item: any) => {
-      const tempDate = dayjs(item.startDate).format("YYYY-MM-DD");
-      const index = dateList.findIndex((d) => d.date === tempDate);
-      console.log(index);
-      if (index > -1) {
-        const tFoundIndex = tempList.findIndex((d) => d.date === tempDate);
-        if (tFoundIndex > -1) {
-          tempList[tFoundIndex].list = [...tempList[tFoundIndex].list, item];
-        } else {
-          tempList.push({ date: tempDate, list: [item] });
-        }
-      }
-    });
-    console.log("temp list", tempList);
-    if (tempList.length > 0) {
-      tempList.map((item) => {
-        setDateList((prev) => {
-          const temp = prev.map((d) => {
-            if (d.date === item.date) {
-              return { ...d, data: item.list };
-            } else {
-              return d;
-            }
-          });
-          return temp;
-        });
-      });
-    }
-    setIsLoaded(true);
-  }, [isMainChar, dateList]);
+  const handleClickTitle = useCallback(
+    (id: number) => {
+      navigate(`/raid/${id}`);
+    },
+    [navigate],
+  );
 
-  useEffect(() => {
-    if (!isLoaded) getData();
-  }, [getData, isLoaded]);
+  const myCalendarViewDatesProps: MyCalendarViewDatesProps = useMemo(
+    () => ({
+      selectedDates,
+      handlePrevTurn,
+      handleNextTurn,
+      handleSelectDate,
+    }),
+    [selectedDates, handlePrevTurn, handleNextTurn, handleSelectDate],
+  );
+
+  const myCalendarViewListProps: MyCalendarViewListProps = useMemo(
+    () => ({
+      listBySelectedDate,
+      handleClickTitle,
+    }),
+    [listBySelectedDate, handleClickTitle],
+  );
 
   return (
-    // <div>
-    //   <Calendar date={date} setDate={setDate} highlighted={highlighted} />
-    // </div>
-    <div>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <button
-          onClick={() => {
-            setDateTurn((prev) => {
-              if (prev > 0) {
-                return prev - 1;
-              } else {
-                return lastTurn;
-              }
-            });
-          }}
-        >
-          minus
-        </button>
-        {selectedDates.map((d) => (
-          <div
-            key={d.date}
-            onClick={() => {
-              setSelectedDate(d.date);
-            }}
-          >
-            <div>
-              {dayjs(d.date).date()}일
-              {d.start ? ` (${dayjs(d.date).month()}월)` : ""}
-            </div>
-            {d.data.length > 0 && (
-              <div>
-                {d.data.map((item) => (
-                  <div key={item.board_number}>{item.raid_type}</div>
-                ))}
-              </div>
-            )}
-          </div>
-        ))}
-        <button
-          onClick={() => {
-            setDateTurn((prev) => {
-              if (prev < lastTurn) {
-                return prev + 1;
-              } else {
-                return 0;
-              }
-            });
-          }}
-        >
-          plus
-        </button>
-      </div>
-      <div>
-        {selectedDate && (
-          <div>
-            {dateList
-              .find((d) => d.date === selectedDate)
-              ?.data.map((item) => (
-                <div key={`${item.board_number}_${item.startDate}`}>
-                  {item.title}
-                </div>
-              ))}
-          </div>
-        )}
-      </div>
-    </div>
+    <MyCalendarView>
+      <MyCalendarView.Dates {...myCalendarViewDatesProps} />
+      <MyCalendarView.List {...myCalendarViewListProps} />
+    </MyCalendarView>
   );
 };
 
---
+++ frontend/src/hooks/useMyArticles.ts
@@ -0,0 +1,180 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import dayjs from "dayjs";
+
+// recoil
+import { useRecoilValue } from "recoil";
+import { mainCharState } from "../atoms/mainCharacter";
+
+// utils
+import fetchUtils from "../utils/fetchUtils";
+
+type IRaidType =
+  | "발탄"
+  | "비아키스"
+  | "쿠크세이튼"
+  | "아브렐슈드"
+  | "일리아칸"
+  | "카멘"
+  | "에키드나"
+  | "상아탑"
+  | "카양겔";
+
+export interface IBoard {
+  board_list: IBoard[] | null;
+  board_number: number;
+  card_level: string;
+  character_name: string | null;
+  content: string;
+  limit: number;
+  maxGate: "1" | "2" | "3" | "4" | "";
+  member: string[] | null;
+  member_count: number;
+  mention: string | null;
+  minGate: "1" | "2" | "3" | "4" | "";
+  offset: number;
+  party_member: string | null;
+  proficiency: "트라이" | "클경" | "반숙" | "숙련";
+  raid_difficulty: "normal" | "hard" | "extreme";
+  raid_leader: string;
+  raid_type: IRaidType;
+  startDate: string;
+  title: string;
+  user_number: string;
+}
+
+export interface IDate {
+  date: string;
+  start: boolean;
+  data: IBoard[];
+}
+
+const dates: IDate[] = [];
+let month: number = 0;
+
+for (let i = 0; i < 30; i++) {
+  const date = dayjs().add(i, "day");
+  const m = date.month();
+  const item = {
+    date: dayjs().add(i, "day").format("YYYY-MM-DD"),
+    start: m !== month,
+    data: [],
+  };
+  dates.push(item);
+  month = m;
+}
+
+// 7 items in one turn
+const COUNT = 7;
+const lastTurn = Math.floor(dates.length / COUNT);
+
+const useMyArticles = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const isMainChar = useRecoilValue(mainCharState);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [dateList, setDateList] = useState<IDate[]>(dates);
+  const [dateTurn, setDateTurn] = useState(0);
+  const selectedDates = useMemo(
+    () => dateList.slice(dateTurn * COUNT, dateTurn * COUNT + COUNT),
+    [dateList, dateTurn],
+  );
+  const [selectedDate, setSelectedDate] = useState(
+    searchParams.get("date") || "",
+  );
+  function handleSelectDate(date: string) {
+    if (date.length === 0) return;
+    setSelectedDate(date);
+    setSearchParams({ date });
+  }
+  const listBySelectedDate = useMemo(
+    () => dateList.find((d) => d.date === selectedDate)?.data || [],
+    [dateList, selectedDate],
+  );
+
+  function handlePrevTurn() {
+    setDateTurn((prev) => {
+      if (prev > 0) {
+        return prev - 1;
+      } else {
+        return lastTurn;
+      }
+    });
+  }
+  function handleNextTurn() {
+    setDateTurn((prev) => {
+      if (prev < lastTurn) {
+        return prev + 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  const getData = useCallback(async () => {
+    if (!isMainChar.user_number) {
+      alert("로그인이 필요한 페이지입니다.");
+      navigate("/");
+      return;
+    }
+    const { data, success } = await fetchUtils.post("/board/getRaid", {
+      user_number: isMainChar.user_number,
+    });
+    if (!success) return;
+    if (!dateList) return;
+
+    const tempList: { date: string; list: any[] }[] = [];
+
+    data.boardModel.board_list.map((item: IBoard) => {
+      const tempDate = dayjs(item.startDate).format("YYYY-MM-DD");
+      const index = dateList.findIndex((d) => d.date === tempDate);
+
+      if (index > -1) {
+        const tFoundIndex = tempList.findIndex((d) => d.date === tempDate);
+        if (tFoundIndex > -1) {
+          tempList[tFoundIndex].list = [...tempList[tFoundIndex].list, item];
+        } else {
+          tempList.push({ date: tempDate, list: [item] });
+        }
+      }
+    });
+
+    if (tempList.length > 0) {
+      tempList.map((item) => {
+        setDateList((prev) => {
+          const temp = prev.map((d) => {
+            if (d.date === item.date) {
+              return { ...d, data: item.list };
+            } else {
+              return d;
+            }
+          });
+          return temp;
+        });
+      });
+    }
+    setIsLoaded(true);
+  }, [isMainChar, dateList, navigate]);
+
+  useEffect(() => {
+    if (!isLoaded) getData();
+  }, [getData, isLoaded]);
+
+  useEffect(() => {
+    const dateParam = searchParams.get("date") || "";
+    if (dateParam.length > 0) {
+      setSelectedDate(dateParam);
+    }
+  }, [searchParams]);
+
+  return {
+    handleNextTurn,
+    handlePrevTurn,
+    selectedDates,
+    setSelectedDate,
+    listBySelectedDate,
+    handleSelectDate,
+  };
+};
+
+export default useMyArticles;
