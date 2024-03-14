024863582ba94f75e4dc25bcb72aebc39d96618a
Wille Lee <1992season@gmail.com>
2024-03-14 20:37:31 +0900

    [EDIT] header

--- frontend/src/components/layouts/Header/index.tsx
+++ frontend/src/components/layouts/Header/index.tsx
@@ -1,35 +1,56 @@
-import React, { useCallback, useMemo, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 // recoil
 import { useRecoilState, useRecoilValue } from "recoil";
 import { loginState } from "@atoms/login";
-import { screenModeActions, screenModeState } from "@/atoms/screenModeState";
+import { screenModeActions, screenModeState } from "@atoms/screenModeState";
+
+// hooks
+import useClickOutside from "@hooks/useClickOutside";
 
 // utils
-import cookies from "@/utils/cookies";
+import cookies from "@utils/cookies";
+import loginUtils from "@utils/loginUtils";
 
 // components
-import loginUtils from "@utils/loginUtils";
-import HeaderView, { HeaderViewButtonsProps } from "./HeaderView";
-import useClickOutside from "@/hooks/useClickOutside";
-import { useNavigate } from "react-router-dom";
+import HeaderView, {
+  HeaderViewButtonsProps,
+  HeaderViewLogosProps,
+} from "./HeaderView";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function Header({ ...props }: Props) {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  function handleClickLogo() {
+  const handleClickLogo = useCallback(() => {
     navigate("/");
-  }
+  }, [navigate]);
+
   const isLoggedin = useRecoilValue(loginState);
+  const [showNavModal, setShowNavModal] = useState(false);
   const [showSetting, setShowSetting] = useState(false);
   const [screenMode, setScreenMode] = useRecoilState(screenModeState);
+  const navModalRef = useRef<HTMLDivElement>(null);
   const settingRef = useRef<HTMLDivElement>(null);
+  const toggleNavModal = () => {
+    setShowNavModal((prev) => !prev);
+  };
+  const handleCloseNavModal = () => {
+    setShowNavModal(false);
+  };
   const toggleSetting = () => {
+    setShowNavModal(false);
     setShowSetting((prev) => !prev);
   };
-
   const handleClickOutsideSetting = useCallback(() => {
     if (!showSetting) return;
     setShowSetting(false);
@@ -49,30 +70,40 @@ export default function Header({ ...props }: Props) {
     loginUtils.logoutKakao();
   }
 
-  useClickOutside(settingRef, handleClickOutsideSetting);
-
   const headerViewButtonsProps: HeaderViewButtonsProps = useMemo(
     () => ({
+      isDark: screenMode === "dark",
+      showSetting,
       isLoggedin,
+      toggleScreenMode,
       login,
       logout,
       toggleSetting,
     }),
-    [isLoggedin],
+    [isLoggedin, showSetting, toggleScreenMode, screenMode],
   );
+
+  const headerViewLogosProps: HeaderViewLogosProps = useMemo(
+    () => ({
+      showNavModal,
+      handleClickLogo,
+      toggleNavModal,
+      handleCloseNavModal,
+    }),
+    [showNavModal, handleClickLogo],
+  );
+
+  useClickOutside(settingRef, handleClickOutsideSetting);
+
+  useEffect(() => {
+    setShowNavModal(false);
+  }, [location]);
 
   return (
     <HeaderView {...props}>
-      <HeaderView.Logos handleClickLogo={handleClickLogo} />
+      <HeaderView.Logos {...headerViewLogosProps} ref={navModalRef} />
       <HeaderView.CharacterList />
-      <HeaderView.Buttons {...headerViewButtonsProps} />
-      {showSetting && (
-        <HeaderView.Setting
-          isDark={screenMode === "dark"}
-          toggleScreenMode={toggleScreenMode}
-          ref={settingRef}
-        />
-      )}
+      <HeaderView.Buttons {...headerViewButtonsProps} ref={settingRef} />
     </HeaderView>
   );
 }
