0218e9d13ac56a48744e87d63c0496a0f63c2aa2
Wille Lee <1992season@gmail.com>
2024-03-14 21:29:53 +0900

    [EDIT] logout

--- frontend/src/utils/loginUtils.ts
+++ frontend/src/utils/loginUtils.ts
@@ -1,9 +1,5 @@
-import cookieUtils from "./cookieUtils";
 const REDIRECT_URL = import.meta.env.VITE_APP_REDIRECT_URL;
 const loginUtils = {
-  isLoggedin: () => {
-    return cookieUtils.getCoookie("access_token") !== undefined;
-  },
   loginKakao: () => {
     window.Kakao.Auth.authorize({
       redirectUri: REDIRECT_URL,
